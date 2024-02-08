f877a5a63a048715daf149667b65243693259ad1
szabinakov <szabina@charisma.ai>
2024-02-08 14:53:12 +0000

    name update, new release

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,6 +1,6 @@
+import type { DocsThemeConfig } from '@charisma-ai/nextra-theme-docs'
+import { useConfig } from '@charisma-ai/nextra-theme-docs'
 import { useRouter } from 'next/router'
-import type { DocsThemeConfig } from 'nextra-theme-docs'
-import { useConfig } from 'nextra-theme-docs'
 
 const logo = (
   <svg
